7007d65aadbc765cccf4df06af0520b21d21ac8e
Leslie Al-Hassan <leslie.k.alhassan@gmail.com>
2023-11-17 01:18:24 -0500

    Remove commented code

--- SOFTWARE/client/src/components/HomePage/HomePage.jsx
+++ SOFTWARE/client/src/components/HomePage/HomePage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
         </a>
       </header>
 
-      {/* main nav s*/}
+      {/* main nav */}
       <Nav />
 
       {/* MAIN CONTENT */}
@@ -27,30 +27,10 @@ const HomePage = () => {
         <Sidebar />
 
         <section className='main__content'>
-          {/* <article className='about'> */}
           <h1 className='main__title'>
             Daily Shelter & Overnight Service Occupancy & Capacity
           </h1>
 
-          {/* <p className='about__description'>
-              This data set provides a daily list of active overnight shelter
-              and allied services in the Shelter Support and Housing
-              Administration division's Shelter Management Information System
-              (SMIS) database. The data provides daily updated information about
-              shelter and overnight service programs administered by SSHA
-              including the program's operator, location, classification,
-              occupancy and capacity. This reporting revises and updates the
-              approach taken in the{' '}
-              <a
-                className='about__description__link'
-                href='/'
-              >
-                Daily Shelter Occupancy data set
-              </a>
-              , starting with the current year data for 2021. This new data set
-              includes the following revisions:
-            </p> */}
-          {/* </article> */}
           <AccordionComponent />
         </section>
       </main>
