b93f7d341c8a648d9a908c373b55acfe27b598bd
Ami Massarella <ami.massarella@gmail.com>
2023-11-17 13:26:20 -0500

    Sidebar styling

--- SOFTWARE/client/src/components/Sidebar/Sidebar.jsx
+++ SOFTWARE/client/src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,64 @@
 import React from 'react';
 import './Sidebar.scss';
+import Calendar from '../../assets/images/calendar.svg';
+import Stack from "../../assets/images/stack.svg";
+import Person from "../../assets/images/person.svg";
+import Info from "../../assets/images/info.svg";
+import Speed from "../../assets/images/speed.svg";
+import Trophy from "../../assets/images/trophy.svg";
+
 
 const Sidebar = () => {
   return (
     <section className='sidebar'>
-      <h4>Details</h4>
+
+<div className="sidebar__grade sidebar__card"><span className="sidebar__header">Grade <img src={Info}/></span>
+<p className="p__gold"><img className="img__Trophy1"src={Trophy} />GOLD</p>
+</div>
+      <div className="sidebar__overall-score sidebar__card"><span className="sidebar__header">Overall Score <img src={Info}/></span>
+      <p className="p__overall-score">100%</p></div>
+      <div className="sidebar__last-refreshed"><span className="sidebar__header">Last Refreshed</span>
+      <p className="p__last-refreshed">Nov 14, 2023</p></div>
+      
+      <h4 className="h4__details"><img src={Stack}/> Details</h4>
 
       <div className='sidebar__details'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
-        facilis doloribus quasi consectetur omnis alias temporibus reiciendis
-        perspiciatis in est ab blanditiis laudantium molestias pariatur
-        veritatis, nisi voluptatem nobis perferendis? Dolor, itaque aspernatur
-        quos provident delectus laborum autem dolores laudantium, omnis totam
-        ipsam deserunt, quis incidunt magnam mollitia recusandae inventore. Sunt
-        nobis reprehenderit vel necessitatibus tempora quis expedita tenetur
-        rerum! Assumenda rerum mollitia iure sequi voluptates, commodi nulla
-        asperiores distinctio iusto corporis velit aliquid, laudantium porro eos
-        odio ea. Pariatur similique facere totam! Nemo deleniti, atque quidem
-        harum ipsum repellat. Eos, earum veritatis? Libero, vero blanditiis
-        numquam mollitia facilis eum, nostrum dolores nemo nulla sed ad atque
-        magnam voluptates ipsum delectus, impedit optio eligendi? Esse sequi
-        nulla sunt autem nisi. Eligendi molestiae alias non maxime veritatis
-        molestias quo nostrum voluptatibus culpa tenetur, pariatur consequuntur
-        aliquid eaque minus. A magnam totam eveniet qui quo blanditiis est
-        repellat in, ullam optio maiores?
+      <div className="div__flex"><span>Data quality score <img src=""/></span><span className="span__beta">beta</span></div>
+     <p><img className="img__Trophy2" src={Trophy}/>Gold</p>
+     <span>Data last refreshed <img src=""/></span>
+     <span>Refresh schedule <img src=""/></span>
+     <p>Daily</p>
+     <span>Data type <img src=""/></span>
+     <p>Table</p>
+     <span>Topics <img src=""/></span>
+     <p>City government, Community services</p>
+     <span>More information <img src=""/></span>
+     <p>External links</p>
+     <span>License <img src=""/></span>
+     <p>Open Government <br></br>License - Toronto</p>
       </div>
+
+      
+
+      <h4 className="h4__details"><img src={Person}/> Publisher</h4>
+      <div className='sidebar__publisher'>
+      <span>Published by <img src=""/></span>
+      <p>Shelter, Support & <br></br>Housing Administration</p>
+     <span>Contact <img src=""/></span>
+     <p>sshadata@toronto.ca</p>
+    
+      </div>
+
+      
+
+
+
+
+  
+
+    
+     
+
     </section>
   );
 };
