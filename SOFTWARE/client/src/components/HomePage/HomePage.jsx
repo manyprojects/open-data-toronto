a0ad4d4af2e4a8b963b57e98037ab7fea91fe683
Leslie Al-Hassan <leslie.k.alhassan@gmail.com>
2023-11-16 23:39:18 -0500

    Extract Accordion into component

--- SOFTWARE/client/src/components/HomePage/HomePage.jsx
+++ SOFTWARE/client/src/components/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Map from '../Map/Map';
 import './HomePage.scss';
-import Accordion from 'react-bootstrap/Accordion';
+import AccordionComponent from '../AccordionComponent/AccordionComponent';
 
 const HomePage = () => {
   return (
@@ -20,82 +19,7 @@ const HomePage = () => {
       {/* MAIN CONTENT */}
       <main className='main'>
         <section className='main__content'>
-          <Accordion
-            defaultActiveKey={['0']}
-            alwaysOpen
-          >
-            <Accordion.Item eventKey='0'>
-              <Accordion.Header>DATA PREVIEW</Accordion.Header>
-              <Accordion.Body>
-                <Map />
-              </Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey='1'>
-              <Accordion.Header>DATA FEATURES</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey='2'>
-              <Accordion.Header>DATA QUALITY</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey='3'>
-              <Accordion.Header>DOWNLOAD DATA</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey='4'>
-              <Accordion.Header>EXPLORE DATA</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-
-            <Accordion.Item eventKey='5'>
-              <Accordion.Header>FOR DEVELOPERS</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
+          <AccordionComponent />
         </section>
       </main>
     </>
