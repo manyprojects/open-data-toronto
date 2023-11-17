211207e78318703a3d90461c058909be7bb34651
Leslie Al-Hassan <leslie.k.alhassan@gmail.com>
2023-11-17 11:38:29 -0500

    Add search

--- SOFTWARE/client/src/components/Map/Map.jsx
+++ SOFTWARE/client/src/components/Map/Map.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import './Map.scss';
+import graphOne from '../../assets/graphs/trendline_occupancy.png';
+import Places from '../Places/Places';
 import {
   GoogleMap,
   Marker,
@@ -15,6 +17,13 @@ const options = {
 };
 
 const Map = () => {
+  const mapRef = useRef();
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+
+  const [location, setLocation] = useState(null);
+
+  console.log(location);
+
   return (
     <div className='container'>
       <div className='stats'>
@@ -33,33 +42,41 @@ const Map = () => {
         </div>
       </div>
 
+      <Places
+        onSetLocation={setLocation}
+        setLocation={(location) => {
+          setLocation(location);
+          mapRef.current?.panTo(location);
+        }}
+      />
+
       <div className='map'>
         <GoogleMap
           zoom={10}
           center={center}
-          options={options}
+          // options={options}
           mapContainerClassName='map-container'
-        >
-          <Marker
-            position={center}
-            icon='https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-          />
-        </GoogleMap>
+          onLoad={onLoad}
+        ></GoogleMap>
       </div>
 
       <div className='stats'>
         <div className='stats__three'>
-          <h1 className='stat__title'>Graph 1</h1>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, eius.
-          Aliquid natus aliquam quaerat, doloribus, magni temporibus cumque
-          repellat et similique dolor qui. Maxime, alias dolore? Dicta quidem
-          amet rem!
+          <h4 className='stat__title'>Weekly Average Bed Occupancy Rate</h4>
+          <img
+            src={graphOne}
+            alt='graph'
+            className='graph__one'
+          />
         </div>
         <div className='stats__four'>
           <h1 className='stat__title'>Graph 2</h1>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus porro
-          rem, reiciendis, nihil est quae voluptate maxime iure assumenda fugiat
-          alias? Magnam aliquam, autem consequatur ut numquam minus qui porro.
+
+          <img
+            src={graphOne}
+            alt='graph'
+            className='graph__one'
+          />
         </div>
       </div>
     </div>
