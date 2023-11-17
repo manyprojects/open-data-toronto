56c1223e08b34e9c0cc4fb5c00535c75ebb0f3d3
Leslie Al-Hassan <leslie.k.alhassan@gmail.com>
2023-11-16 23:55:50 -0500

    Add unstyled nav

--- SOFTWARE/client/src/components/HomePage/HomePage.jsx
+++ SOFTWARE/client/src/components/HomePage/HomePage.jsx
@@ -16,6 +16,72 @@ const HomePage = () => {
         </a>
       </header>
 
+      <nav className='nav'>
+        <ul className='nav__list'>
+          <li className='nav__list__item'>
+            <a
+              className='nav__link'
+              href=''
+            >
+              OPEN DATA
+            </a>
+          </li>
+
+          <li className='nav__list__item'>
+            <a
+              className='nav__link'
+              href=''
+            >
+              Data Catalogue
+            </a>
+          </li>
+
+          <li className='nav__list__item'>
+            <a
+              className='nav__link'
+              href=''
+            >
+              Knowledge Centre
+            </a>
+          </li>
+
+          <li className='nav__list__item'>
+            <a
+              className='nav__link'
+              href=''
+            >
+              About
+            </a>
+          </li>
+
+          <li className='nav__list__item'>
+            <a
+              className='nav__link'
+              href=''
+            >
+              Gallery
+            </a>
+          </li>
+
+          <li className='nav__list__item'>
+            <a
+              className='nav__link'
+              href=''
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <input
+          className='nav__search'
+          type='search'
+          placeholder='Search'
+        />
+
+        <button className='nav__button'>SEARCH</button>
+      </nav>
+
       {/* MAIN CONTENT */}
       <main className='main'>
         <section className='main__content'>
